575c3a08e1fb0f35f3a849030bc8694a2077d5e2
Ben Edwards <benedwrds@yahoo.com>
2021-02-08 18:29:06 -0600

    heroku setup

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require('path')
 
 const users = require("./routes/api/users");
 
 const app = express();
 // Bodyparser middleware
+
 app.use(
   bodyParser.urlencoded({
     extended: false
@@ -34,6 +36,14 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
   
   const port = process.env.PORT || 5000; 
   // process.env.port is Heroku's port if you choose to deploy the app there 
